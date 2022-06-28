a829a77a533d8742aedbacfc7d99ac5671a877ac
nellizi <masterliz9095@gmail.com>
2022-06-28 14:50:24 +0900

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
-import * as React from "react"
+
+import React from "react";
+import Layout from "../components/layout";
+
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Home Page</h1>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    
+      />
+    </Layout>
   );
 };
 
