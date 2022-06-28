0f7fa6644498699e4a765dec29bcd261459d17e4
nellizi <masterliz9095@gmail.com>
2022-06-28 17:22:49 +0900

    일단 완성..

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
-
 import React from "react";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    
-      />
+     
     </Layout>
   );
 };
